74a0884bcde1ca601ae4ad53a1b7cf83c0ff98eb
leandrosboukas <leandrosboukas@leandrosboukass-MacBook-Pro-3.local>
2025-06-20 13:58:51 -0400

    updated function that outputs de novo granges to allow filtering based on genomic annotation

--- paper_analysis_code/paper_revision_analysis/de_novo_variant_granges.R
+++ paper_analysis_code/paper_revision_analysis/de_novo_variant_granges.R
@@ -1,22 +1,39 @@
-getDeNovoVariantGRanges <- function(duoNovo_granges_output_filepath, duo_type = c("PM", "PF")){
+getDeNovoVariantGRanges <- function(duoNovo_granges_output_filepath, duo_type = c("PM", "PF"), 
+                                    filter_problematic_regions = TRUE, 
+                                    exclude_clustered_denovos = TRUE,
+                                    genomic_context = NULL){
   load(file = duoNovo_granges_output_filepath)
   if (duo_type == "PF"){
     dn_granges <- dn_granges_pf
   } else if (duo_type == "PM"){
     dn_granges <- dn_granges_pm
   }
-  dn_granges$problematic_region <- as.logical(dn_granges$problematic_region)
-  dn_granges <- dn_granges[which(dn_granges$problematic_region == FALSE)]
   dn_granges <- dn_granges[which(dn_granges$phasing_parent == "0/0" & dn_granges$GQ_parent >= 40)]
-  
+  if (filter_problematic_regions == TRUE){
+    dn_granges$problematic_region <- as.logical(dn_granges$problematic_region)
+    dn_granges <- dn_granges[which(dn_granges$problematic_region == FALSE)]
+  }
+  if (exclude_clustered_denovos == TRUE){
+    clustered <- which(dn_granges$n_de_novo_left_orientation_same_PS > 1 | 
+                         dn_granges$n_de_novo_right_orientation_same_PS > 1)
+    if(length(clustered) > 0){
+      dn_granges <- dn_granges[-clustered]
+    }
+  }
   classified_dn <- which(dn_granges$duoNovo_classification == "de_novo" & 
-                           dn_granges$GQ_proband >= 40 &
-                           (dn_granges$n_de_novo_left_orientation_same_PS == 1 | 
-                              dn_granges$n_de_novo_right_orientation_same_PS == 1))
+                           dn_granges$GQ_proband >= 40)
   if (length(classified_dn) > 0){
     dn_granges <- dn_granges[classified_dn]
   } else {
     dn_granges <- GRanges()
+  }
+  if (!is.null(genomic_context)){
+    variants_in_genomic_context <- which(dn_granges$Func.refGeneWithVer %in% genomic_context)
+    if (length(variants_in_genomic_context) > 0){
+      dn_granges <- dn_granges[variants_in_genomic_context]
+    } else {
+      dn_granges <- GRanges()
+    }
   }
   dn_granges
 }
