71496881bbbc333692a0c632a086f381c165133c
leandrosboukas <leandrosboukas@OSXLAP13059.local>
2025-07-20 17:14:19 -0400

    added sensitivity figure scripts

---
+++ paper_analysis_code/paper_revision_analysis/sensitivity_aggregate_figures.R
@@ -0,0 +1,94 @@
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) != 2) {
+  stop("Usage: Rscript sensitivity_aggregate_figures.R <data_directory> <figure_directory>")
+}
+
+data_directory   <- args[1]
+figure_directory <- args[2]
+
+# ensure figure directory exists
+if (!dir.exists(data_directory)) {
+  stop(sprintf("Data directory not found: '%s'", data_directory))
+}
+
+setwd(data_directory)
+all_dirs <- list.files()
+
+trio_dn_count <- sapply(1:length(all_dirs), function(xx) {
+  setwd(all_dirs[xx])
+  all_data <- list.files()
+  load(file = all_data[grep("sensitivity", all_data)])
+  setwd(data_directory)
+  
+  sens_both['total']
+})
+
+problematic_trios <- which(trio_dn_count >= 100)
+all_dirs_no_problematic <- all_dirs[-problematic_trios]
+
+sensitivity_mat_pf <- matrix(NA, ncol = length(all_dirs_no_problematic), nrow = 3)
+rownames(sensitivity_mat_pf) <- c('dn', 'other_parent', 'uncertain')
+sensitivity_mat_pm <- sensitivity_mat_pf
+
+for (i in 1:length(all_dirs_no_problematic)){
+  setwd(all_dirs_no_problematic[i])
+  all_data <- list.files()
+  load(file = all_data[grep('sensitivity', all_data)])
+  
+  sensitivity_mat_pf['dn', i] <- sens_pf['de_novo']
+  sensitivity_mat_pf['other_parent', i] <- sens_pf['on_other_parent_haplotype']
+  sensitivity_mat_pf['uncertain', i] <- sens_pf['uncertain']
+  
+  sensitivity_mat_pm['dn', i] <- sens_pm['de_novo']
+  sensitivity_mat_pm['other_parent', i] <- sens_pm['on_other_parent_haplotype']
+  sensitivity_mat_pm['uncertain', i] <- sens_pm['uncertain']
+  
+  setwd(data_directory)
+}
+
+aggregate_pf <- rowSums(sensitivity_mat_pf)
+aggregate_pm <- rowSums(sensitivity_mat_pm)
+
+candidate_percentages_f <- aggregate_pf[1:3]/sum(aggregate_pf)
+candidate_percentages_m <- aggregate_pm[1:3]/sum(aggregate_pm)
+
+pdf(file = paste0(figure_directory, "/pf_aggregate_sensitivity.pdf"), 
+    height = 1.8, width = 2.4, pointsize = 8)
+data <- data.frame(
+  category = names(candidate_percentages_f),
+  value = candidate_percentages_f
+)
+data$category <- fct_relevel(data$category, "uncertain", "other_parent", "dn")
+
+ggplot(data, aes(x = "", y = value, fill = category)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  scale_fill_manual(values = c(alpha("dark gray", 0.75), alpha("forest green", 0.84), "dark orange")) +
+  labs(title = "",
+       x = "",
+       y = "Percentage",
+       fill = "") +
+  theme_classic() +
+  theme(legend.position = "bottom")
+dev.off()
+
+pdf(file = paste0(figure_directory, "/pm_aggregate_sensitivity.pdf"), 
+    height = 1.8, width = 2.4, pointsize = 8)
+data <- data.frame(
+  category = names(candidate_percentages_m),
+  value = candidate_percentages_m
+)
+data$category <- fct_relevel(data$category, "uncertain", "other_parent", "dn")
+
+ggplot(data, aes(x = "", y = value, fill = category)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  scale_fill_manual(values = c(alpha("dark gray", 0.75), alpha("forest green", 0.84), "dark orange")) +
+  labs(title = "",
+       x = "",
+       y = "Percentage",
+       fill = "") +
+  theme_classic() +
+  theme(legend.position = "bottom")
+dev.off()
+
---
+++ paper_analysis_code/paper_revision_analysis/sensitivity_per_trio_figures.R
@@ -0,0 +1,59 @@
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) != 2) {
+  stop("Usage: Rscript sensitivity_per_trio_figures.R <data_directory> <figure_directory>")
+}
+
+data_directory   <- args[1]
+figure_directory <- args[2]
+
+# ensure figure directory exists
+if (!dir.exists(data_directory)) {
+  stop(sprintf("Data directory not found: '%s'", data_directory))
+}
+
+setwd(data_directory)
+all_dirs <- list.files()
+
+trio_dn_count <- sapply(1:length(all_dirs), function(xx) {
+  setwd(all_dirs[xx])
+  all_data <- list.files()
+  load(file = all_data[grep("sensitivity", all_data)])
+  setwd(data_directory)
+  
+  sens_both['total']
+})
+
+problematic_trios <- which(trio_dn_count >= 100)
+all_dirs_no_problematic <- all_dirs[-problematic_trios]
+
+### Sensitivity
+sens <- rep(NA, length(all_dirs_no_problematic))
+sens_no_filter <- sens
+
+for (i in 1:length(all_dirs_no_problematic)){
+  setwd(all_dirs_no_problematic[i])
+  all_data <- list.files()
+  load(file = all_data[grep('sensitivity', all_data)])
+  
+  sens[i] <- sens_both[1]/sens_both[4]
+  sens_no_filter[i] <- sens_both_no_ad_filter[1]/sens_both_no_ad_filter[4]
+  
+  setwd(data_directory)
+}
+
+pdf(file = paste0(figure_directory, "/sensitivity.pdf"), height = 2.4, width = 6.5, pointsize = 8)
+par(mfrow = c(1, 3))
+hist(trio_dn_count, breaks = 50, xlim = c(0, 700), col = "red", border = "white", 
+     xlab = "# de novos from trio", main = "")
+plot(trio_dn_count, sens, xlab = "# de novos from full trio", ylab = "sensitivity (both duos combined)", 
+     bty = 'l', yaxt = 'n', xaxt = 'n', ylim = c(0, 0.75))
+axis(1, at = seq(100, 600, by = 100))
+axis(2, at = c(0.25, 0.5, 0.75))
+
+plot(sens_no_filter, sens, xlab = "sensitivity (w/out allele depth filter)", 
+     ylab = "sensitivity (with allele depth filter)", 
+     bty = 'l', yaxt = 'n', xaxt = 'n', ylim = c(0, 0.75), xlim = c(0, 0.75))
+axis(1, at = c(0.25, 0.5, 0.75))
+axis(2, at = c(0.25, 0.5, 0.75))
+abline(0, 1, lwd = 1.75, col = 2)
+dev.off()
