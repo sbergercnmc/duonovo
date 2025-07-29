588b27b047ee80d4aac0af49f2d28f05c577fca7
leandrosboukas <leandrosboukas@OSXLAP13059.local>
2025-07-29 15:56:48 -0400

    updated script for psm trio result analysis

--- paper_analysis_code/proband_sibling_mother_trio_analysis/run_duoNovo_sib.R
+++ paper_analysis_code/proband_sibling_mother_trio_analysis/run_duoNovo_sib.R
@@ -3,37 +3,21 @@ source("getHaplotypesAndBlockBoundariesForTrio.R")
 source("classifyVariantsTrio.R")
 
 library(VariantAnnotation)
-setwd('duoNovoOutputs_ANNOVAR')
-directories <- list.dirs(path = ".", full.names = FALSE, recursive = FALSE)
-
-# retrieve command-line arguments
 args <- commandArgs(trailingOnly = TRUE)
-dir_index <- as.integer(args[1]) #will be used later
-setwd(directories[dir_index])
+if (length(args) != 2L) {
+  stop("Usage: run_duoNovo_sib.R <run_directory>")
+}
 
-duoNovo_output_filepaths <- list.files( #output from process_duoNovo_output.R
-  path       = "duoNovoOutputsWithOtherParentGeno/PM", #replace with correct directory if necessary
-  pattern    = "duoNovo\\.annovar\\.rda$",
-  full.names = TRUE
-)
-duoNovo_output_filepath <- duoNovo_output_filepaths[dir_index]
-
-actual_trio_denovo_filepaths <- list.files( #output from get_denovo_from_trio.R
-  path        = "trio_de_novo", #replace with correct directory if necessary
-  pattern     = "^TRIO.*annovar\\.rda$",
-  full.names  = TRUE
-)
-actual_trio_denovo_filepath <- actual_trio_denovo_filepaths[dir_index]
+psm_trio_dir <- args[1]
+duo_dir <- args[2]
 
 ### use duoNovo output from the mother-proband duo to separate candidate variants based on classification
-load(file = duoNovo_output_filepath) 
+setwd(duo_dir)
+duonovo_granges_output_filepath <- list.files(pattern = "PM\\.duonovo\\.annovar\\.addedParent\\.dnm2.rda$")
+load(file = duonovo_granges_output_filepath)
 duoNovo_ranges <- dn_granges_pm
-duoNovo_ranges$giab_problematic <- unlist(duoNovo_ranges$giab_problematic)
-duoNovo_ranges <- duoNovo_ranges[which(duoNovo_ranges$giab_problematic == ".")]
 
-classified_denovo_indices <- which(duoNovo_ranges$duoNovo_classification == "de_novo" & 
-                         (duoNovo_ranges$n_de_novo_left_orientation_same_PS == 1 | 
-                            duoNovo_ranges$n_de_novo_right_orientation_same_PS == 1))
+classified_denovo_indices <- which(duoNovo_ranges$duoNovo_classification == "de_novo")
 if (length(classified_denovo_indices) > 0){
   classified_denovo <- duoNovo_ranges[classified_denovo_indices]
 } else {
@@ -47,14 +31,13 @@ classified_other_parent_or_uncertain <- duoNovo_ranges[c(classified_uncertain_in
 
 ### Now run duoNovo (trio version) on the joint trio vcf (proband-sibling-mother; samples have to be ordered this way in the vcf) 
 ### to classify the candidate variants labeled as "on other parent haplotype" or uncertain
-surrogate_trio_vcf_filepath <- list.files(pattern = "TRIO_PSM")
+setwd(trio_dir)
+surrogate_trio_vcf_filepath <- list.files(pattern = "PSM\\.vcf.gz$")
 sibling_trio_output <- duoNovoSib(surrogate_trio_vcf_filepath, 
                                   candidate_variants_duoNovo_output = classified_other_parent_or_uncertain)
 
 # Obtain de novo variants from the output
-denovo_indices_sibling_trio <- which(sibling_trio_output$duoNovo_classification == "de_novo" & 
-                                   (sibling_trio_output$n_de_novo_left_orientation_same_PS == 1 | 
-                                      sibling_trio_output$n_de_novo_right_orientation_same_PS == 1))
+denovo_indices_sibling_trio <- which(sibling_trio_output$duoNovo_classification == "de_novo")
 if (length(denovo_indices_sibling_trio) > 0){
   denovo_sibling <- sibling_trio_output[denovo_indices_sibling_trio]
   
@@ -70,44 +53,4 @@ if (length(denovo_indices_sibling_trio) > 0){
   denovo_sibling <- GRanges()
 }
 
-### Now import actual trio, and calculate what fraction of the ground truth de novo variants are detected:
-### a) from mother-proband duo only; b) using the proband-sibling-mother trio
-load(file = actual_trio_denovo_filepath) #name of imported file is "trio_de_novo"
-trio_de_novo <- trio_de_novo[which(trio_de_novo$proband_gq >= 40 & 
-                                     trio_de_novo$parent1_gq >= 40 & 
-                                     trio_de_novo$parent2_gq >= 40)]
-trio_de_novo$giab_problematic <- unlist(trio_de_novo$giab_problematic)
-trio_de_novo <- trio_de_novo[which(trio_de_novo$giab_problematic == ".")]
-
-sensitivity_duo_only <- length(unique(queryHits(findOverlaps(trio_de_novo, 
-                                                             classified_denovo))))/ length(trio_de_novo)
-combined_denovos <- c(classified_denovo, denovo_sibling)
-sensitivity_duo_plus_sibling <- length(unique(queryHits(findOverlaps(trio_de_novo, 
-                                                                     combined_denovos))))/ length(trio_de_novo)
-
-### get false positive rate (calculate fraction of de novos detected from the proband-sibling-mother trio
-### that are not de novo when we look at the actual father's genotype)
-if (length(denovo_indices_sibling_trio) > 0){
-  denovo_sibling_false <- unique(queryHits(findOverlaps(denovo_sibling, 
-                                                        duoNovo_ranges[grep("1", duoNovo_ranges$parentValidation_gt)])))
-  false_positive_rate <- length(denovo_sibling_false)/length(denovo_sibling)
-  parental_origin <- table(denovo_sibling$parent_origin)
-} else {
-  false_positive_rate <- NA
-}
-
-### save results
-duo_basename_pm    <- basename(duoNovo_output_filepath)
-output_filename_pm <- paste0(
-  sub("\\.annovar\\.rda$", "", duo_basename_pm),  
-  ".rda"
-)
-
-save(
-  sensitivity_duo_only,
-  sensitivity_duo_plus_sibling,
-  false_positive_rate,
-  parental_origin,
-  file = paste0("sib_sensitivity_", output_filename_pm)
-)
 
