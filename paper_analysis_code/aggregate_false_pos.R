54162e16f8009a0cf6a1869cc42018dbf98e7c77
leandrosboukas <leandrosboukas@OSXLAP13059.local>
2025-10-05 18:33:59 -0400

    cleaned up dir

--- paper_analysis_code/aggregate_false_pos.R
+++
@@ -1,7 +0,0 @@
-aggregate_transmitted_f <- rowSums(false_pos_pf)
-percentages_transmitted_f <- aggregate_transmitted_f/sum(aggregate_transmitted_f)
-per_sample_false_pos_rate_f <- apply(false_pos_pf, 2, function(xx) xx[1]/sum(xx))
-
-aggregate_transmitted_m <- rowSums(false_pos_pm)
-percentages_transmitted_m <- aggregate_transmitted_m/sum(aggregate_transmitted_m)
-per_sample_false_pos_rate_m <- apply(false_pos_pm, 2, function(xx) xx[1]/sum(xx))
